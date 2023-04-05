8f9301ebd0ce951ed1b94407f3ea5932b17f6aa8
elimend <128734627+elimend@users.noreply.github.com>
2023-04-04 22:23:11 -0400

    Create Machine_Learning_Schikit.py
    Python code performing machine learning with Scikit-Learn

---
+++ Machine_Learning_Schikit.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load the Iris dataset
+iris = load_iris()
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+
+# Train a logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model's accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy:', accuracy)
